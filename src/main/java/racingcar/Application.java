003a4899c243e1c4e6293d05b709a344f8bc37a8
danalee <eleeje97@gmail.com>
2023-03-23 09:44:41 +0900

    Feat: 게임시작 메시지 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.view.UIManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UIManager uiManager = new UIManager();
+
+        uiManager.printGameStartMsg();
     }
 }
---
+++ src/main/java/racingcar/view/UIManager.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+public class UIManager {
+    private final ErrorMsgMaker errorMsgMaker;
+
+    public UIManager() {
+        errorMsgMaker = new ErrorMsgMaker();
+    }
+
+    public void printGameStartMsg() {
+        System.out.println("##### GAME START #####");
+    }
+}
