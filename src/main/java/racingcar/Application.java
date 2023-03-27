a8af079747d4db415ac3e67b2de2c936315d425f
danalee <eleeje97@gmail.com>
2023-03-27 09:12:53 +0900

    Feat(RacingGameController): 컨트롤러 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,49 +1,10 @@
 package racingcar;
 
-import racingcar.constants.GameConstants;
-import racingcar.domain.RacingCars;
-import racingcar.domain.TrialCount;
-import racingcar.view.UIManager;
+import racingcar.controller.RacingGameController;
 
 public class Application {
     public static void main(String[] args) {
-        UIManager uiManager = new UIManager();
-        RacingCars racingCars = null;
-        TrialCount trialCount = null;
-
-        uiManager.printGameStartMsg();
-        while (racingCars == null) {
-            racingCars = createRacingCars(uiManager.inputCarNames());
-        }
-
-        while (trialCount == null) {
-            trialCount = createTrialCount(uiManager.inputTrialCount());
-        }
-
-        uiManager.printGameResultMsg();
-        for (int i = 0; i < GameConstants.getTrialCount(); i++) {
-            racingCars.moveCars();
-            uiManager.printCarPositions(racingCars.getCarPositions());
-        }
-
-        uiManager.printWinners(racingCars.pickWinners());
-    }
-
-    public static RacingCars createRacingCars(String userInput) {
-        try {
-            return new RacingCars(userInput);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-    }
-
-    public static TrialCount createTrialCount(String userInput) {
-        try {
-            return new TrialCount(userInput);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+        RacingGameController controller = new RacingGameController();
+        controller.run();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,46 @@
+package racingcar.controller;
+
+import racingcar.constants.GameConstants;
+import racingcar.domain.RacingCars;
+import racingcar.domain.TrialCount;
+import racingcar.view.UIManager;
+
+public class RacingGameController {
+    UIManager uiManager;
+    RacingCars racingCars;
+    TrialCount trialCount;
+
+    public void run() {
+        uiManager = new UIManager();
+
+        uiManager.printGameStartMsg();
+        racingCars = createRacingCars(uiManager.inputCarNames());
+        trialCount = createTrialCount(uiManager.inputTrialCount());
+
+        uiManager.printGameResultMsg();
+        for (int i = 0; i < GameConstants.getTrialCount(); i++) {
+            racingCars.moveCars();
+            uiManager.printCarPositions(racingCars.getCarPositions());
+        }
+
+        uiManager.printWinners(racingCars.pickWinners());
+    }
+
+    public RacingCars createRacingCars(String userInput) {
+        try {
+            return new RacingCars(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createRacingCars(uiManager.inputCarNames());
+        }
+    }
+
+    public TrialCount createTrialCount(String userInput) {
+        try {
+            return new TrialCount(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createTrialCount(uiManager.inputTrialCount());
+        }
+    }
+}
